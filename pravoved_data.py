221ee0202db56f33a718d238424a9ea42993d900
iakovleva <paulina.iakovleva@gmail.com>
2018-12-28 15:25:01 +0200

    Comment steps

--- pravoved_data.py
+++ pravoved_data.py
@@ -15,16 +15,22 @@ def get_income(period):
 
     login_url = tokens.LOGIN_URL
     dashboard_url = tokens.DASHBOARD_URL.format(period)
+
+    # Create session
     session = requests.Session()
     payload = tokens.PAYLOAD
-
+    # Log in
     response = session.post(login_url, data=payload, headers=dict(referer=login_url))
+    # Go to dashboard page
     response = session.get(dashboard_url)
+    # Make soup if login was successful
     if 'Партнерская программа' in response.text:
         soup = bs(response.text, 'html.parser')
         itogo = soup.select('table > tbody > tr')
+        # Find row with income
         i = itogo[-1].select('td')
         pravoved_income = int((i[-1].get_text().strip(' р.')))
         return str(pravoved_income)
+    # Inform if login wasn't successful
     else:
         print('Not logged in')
